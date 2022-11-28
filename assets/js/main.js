8859f154802eb7571a88dd2a52cb7c1fd28369f3
sajjad <sajjad.ardekani@gmail.com>
2022-11-28 18:07:03 +0330

    minor update and add match status

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,184 +1,189 @@
 const content = $('#content');
 
 function init() {
-    initialState();
-    componentMount();
+  initialState();
+  componentMount();
 }
 
 function initialState() {
-    persianDate.toCalendar('gregorian');
-    persianDate.toLocale('en');
-    window.state = {};
-    window.state.data = {};
-    window.state.liveData = true;
-    window.state.loading = true;
-    window.state.rendered = false;
-    window.state.date = new persianDate();
-    window.state.timeoutId = undefined;
+  persianDate.toCalendar('gregorian');
+  persianDate.toLocale('en');
+  window.state = {};
+  window.state.data = {};
+  window.state.liveData = true;
+  window.state.loading = true;
+  window.state.rendered = false;
+  window.state.date = new persianDate();
+  window.state.timeoutId = undefined;
 }
 
 
 function componentMount() {
 
-    fetchData().then(function (res) {
-        window.state.data = res;
-    }).then(function () {
-        if (window.state.rendered === false) {
-            render();
-            window.state.rendered = true;
-        } else {
-            updateComponents();
-        }
-    }).then(function () {
-        if (window.state.loading) {
-            $('.w3samples_table_loader').remove();
-            window.state.loading = false;
-        }
-    }).fail(function (e) {
-        console.log(e)
-    })
-
-    if (getDate()['alis'] === 'today') {
-        window.state.timeoutId = setTimeout(componentMount, 3000);
+  fetchData().then(function (res) {
+    window.state.data = res;
+  }).then(function () {
+    if (window.state.rendered === false) {
+      render();
+      window.state.rendered = true;
+    } else {
+      updateComponents();
     }
+  }).then(function () {
+    if (window.state.loading) {
+      $('.w3samples_table_loader').remove();
+      window.state.loading = false;
+    }
+  }).fail(function (e) {
+    console.log(e)
+  })
+
+  if (getDate()['alis'] === 'today') {
+    window.state.timeoutId = setTimeout(componentMount, 3000);
+  }
 }
 
 
 function render() {
-    let html = '<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">\n';
-    window.state.data.map(function (group) {
-        html += groupComponent(group);
-    });
-    html += '</div>';
-    content.append(html);
+  let html = '<div class="row row-cols-1 row-cols-md-3 mb-3 text-center">\n';
+  window.state.data.map(function (group) {
+    html += groupComponent(group);
+  });
+  html += '</div>';
+  content.append(html);
 
 
-    handelCalender();
+  handelCalender();
 
 }
 
 function handelCalender() {
 
-    const calender = $('#calender-fa');
-    const pwt = $('.pwt-datepicker-input-element');
-    if (!pwt.length) {
-        calender.persianDatepicker({
-            "inline": true,
-            "toolbox": {
-                "enabled": false
-            },
-            onSelect: function (unix) {
-                window.state.rendered = false;
-                window.state.loading = true;
-                clearTimeout(window.state.timeoutId);
-                setData(unix);
-                componentMount();
-            },
-        });
-    }
+  const calender = $('#calender-fa');
+  const pwt = $('.pwt-datepicker-input-element');
+  if (!pwt.length) {
+    calender.persianDatepicker({
+      "inline": true,
+      "toolbox": {
+        "enabled": false
+      },
+      onSelect: function (unix) {
+        window.state.rendered = false;
+        window.state.loading = true;
+        clearTimeout(window.state.timeoutId);
+        setData(unix);
+        componentMount();
+      },
+    });
+  }
 }
 
 function fetchData() {
 
-    if (window.state.loading) {
-        content.empty();
-        content.append(loadingComponent());
-    }
-    const date = getDate();
+  if (window.state.loading) {
+    content.empty();
+    content.append(loadingComponent());
+  }
+  const date = getDate();
 
-    if (!window.state.liveData) {
-        // Get data from Local json file
-        return $.getJSON("content.json", {'s': date['paramDate']}).fail(function () {
-            console.log("An error has occurred.");
-        });
-    } else {
-        // Get live data
-        const url = 'http://varzeshikhan.ir/api/Test/Index';
-        return $.ajax({
-            url: url,
-            type: 'GET',
-            data: {'s': date['paramDate']},
-            headers: {'Access-Control-Allow-Origin': '*'},
-        })
-    }
+  if (!window.state.liveData) {
+    // Get data from Local json file
+    return $.getJSON("content.json", {'s': date['paramDate']}).fail(function () {
+      console.log("An error has occurred.");
+    });
+  } else {
+    // Get live data
+    const url = 'http://varzeshikhan.ir/api/Test/Index';
+    return $.ajax({
+      url: url,
+      type: 'GET',
+      data: {'s': date['paramDate']},
+      headers: {'Access-Control-Allow-Origin': '*'},
+    })
+  }
 }
 
 function updateComponents() {
 
-    window.state.data.map(function (value, index, array) {
-        value.matches.map(function (match) {
-            const scoreHome = $('#match-score-home-' + match.id);
-            const scoreAway = $('#match-score-away-' + match.id);
+  window.state.data.map(function (value, index, array) {
+    value.matches.map(function (match) {
+      const scoreHome = $('#match-score-home-' + match.id);
+      const scoreAway = $('#match-score-away-' + match.id);
+      const matchStatus = $('#match-status-' + match.id);
 
-            if (parseInt(scoreHome.text()) !== match.home.score) {
-                scoreHome.text(match.home.score);
-                scoreHome.addClass('bg-score-updated');
-                setTimeout(function () {
-                    scoreHome.removeClass('bg-score-updated')
-                }, 3000);
-            }
+      if (parseInt(scoreHome.text()) !== match.home.score) {
+        scoreHome.text(match.home.score);
+        scoreHome.addClass('bg-score-updated');
+        setTimeout(function () {
+          scoreHome.removeClass('bg-score-updated')
+        }, 3000);
+      }
 
-            if (parseInt(scoreAway.text()) !== match.away.score) {
-                scoreAway.text(match.away.score);
-                scoreAway.addClass('bg-score-updated');
-                setTimeout(function () {
-                    scoreAway.removeClass('bg-score-updated')
-                }, 3000);
-            }
+      if (parseInt(scoreAway.text()) !== match.away.score) {
+        scoreAway.text(match.away.score);
+        scoreAway.addClass('bg-score-updated');
+        setTimeout(function () {
+          scoreAway.removeClass('bg-score-updated')
+        }, 3000);
+      }
 
-        })
-    });
+      if (match.status.started && !match.status.cancelled && !match.status.finished) {
+        matchStatus.text(match.status.liveTime.short);
+      }
+
+    })
+  });
 }
 
 function setData(unix) {
-    persianDate.toCalendar('gregorian');
-    persianDate.toLocale('en');
-    window.state.date = new persianDate(unix)
+  persianDate.toCalendar('gregorian');
+  persianDate.toLocale('en');
+  window.state.date = new persianDate(unix)
 }
 
 function getDate() {
-    persianDate.toCalendar('gregorian');
-    persianDate.toLocale('en');
+  persianDate.toCalendar('gregorian');
+  persianDate.toLocale('en');
 
-    let now = new persianDate();
-    let date = window.state.date;
+  let now = new persianDate();
+  let date = window.state.date;
 
-    let dateInfo = [];
-    let diff = date.diff(now, 'days');
+  let dateInfo = [];
+  let diff = date.diff(now, 'days');
 
-    if (diff === -1) {
-        dateInfo['alis'] = 'yesterday';
-    } else if (diff === 1) {
-        dateInfo['alis'] = 'tomorrow';
-    } else if (diff === 0) {
-        dateInfo['alis'] = 'today';
-    }
+  if (diff === -1) {
+    dateInfo['alis'] = 'yesterday';
+  } else if (diff === 1) {
+    dateInfo['alis'] = 'tomorrow';
+  } else if (diff === 0) {
+    dateInfo['alis'] = 'today';
+  }
 
-    dateInfo['date'] = date;
-    dateInfo['paramDate'] = date.format('YYYYMMDD');
+  dateInfo['date'] = date;
+  dateInfo['paramDate'] = date.format('YYYYMMDD');
 
-    return dateInfo;
+  return dateInfo;
 }
 
 function activeDateNavbar() {
-    $('.nav-link').removeClass('active')
-    if ($('.nav-link#' + getDate()['alis']).length) {
-        $('.nav-link#' + getDate()['alis']).addClass('active');
-    }
+  $('.nav-link').removeClass('active')
+  if ($('.nav-link#' + getDate()['alis']).length) {
+    $('.nav-link#' + getDate()['alis']).addClass('active');
+  }
 }
 
 
 function groupComponent(group) {
-    let matches = '';
-    group.matches.map(function (match) {
-        matches += matchComponent(match);
-    });
+  let matches = '';
+  group.matches.map(function (match) {
+    matches += matchComponent(match);
+  });
 
-    const groupName = group.groupName !== null ? ' - ' + group.groupName : '';
+  const groupName = group.groupName !== null ? ' - ' + group.groupName : '';
 
-    let html = '';
+  let html = '';
 
-    html += `<table class="table live-scores" id="group-${group.id}">
+  html += `<table class="table live-scores" id="group-${group.id}">
             <caption></caption>
             <thead>
             <tr class="table-info">
@@ -189,76 +194,96 @@ function groupComponent(group) {
             </tr>
             </thead>
             <tbody>`;
-    html += matches;
-    html += `</tbody>
+  html += matches;
+  html += `</tbody>
         </table>`;
-    return html;
+  return html;
 }
 
 function matchComponent(match) {
+  const started = match.status.started;
+  const finished = match.status.finished;
+  const cancelled = match.status.cancelled;
+  let liveTime = !finished && started ? match.status.liveTime.short : finished ? 'Finished' : '';
 
-    return `<tr id="match-${match.id}">
+  let html = '';
+
+  html += `<tr id="match-${match.id}">
                 <td class="text-right">${match.time}</td>
-                <td class="text-left">${match.away.name}</td>
-                <td class="bg-score" id="match-score-away-${match.id}" data-bs-target="${match.away.score}">${match.away.score}</td>
-                <td class="bg-score" id="match-score-home-${match.id}" data-bs-target="${match.home.score}">${match.home.score}</td>
-                <td class="text-right">${match.home.name}</td>
-                <td></td>
-            </tr>`
+                <td class="text-left">${match.away.name}</td>`;
+
+  if (started) {
+    html += `<td class="bg-score" id="match-score-away-${match.id}"
+        data-bs-target="${match.away.score}">${match.away.score}</td>
+    <td class="bg-score" id="match-score-home-${match.id}"
+        data-bs-target="${match.home.score}">${match.home.score}</td>`;
+  } else if (finished) {
+    html += `<td data-bs-target="${match.away.score}">${match.away.score}</td>
+    <td data-bs-target="${match.home.score}">${match.home.score}</td>`;
+  } else {
+    html += `<td></td>
+    <td></td>`;
+  }
+
+
+  html += `<td class="text-right">${match.home.name}</td>
+                <td id="match-status-${match.id}">${liveTime}</td>
+            </tr>`;
+  return html;
 }
 
 function loadingComponent() {
-    return " <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" class=\"w3samples_table_loader\">\n" +
-        "            <tr>\n" +
-        "                <th class=\"col1\">\n" +
-        "                    <span></span>\n" +
-        "                </th>\n" +
-        "                <th class=\"col4\">\n" +
-        "                    <span></span>\n" +
-        "                </th>\n" +
-        "                <th class=\"col5\">\n" +
-        "                    <span></span>\n" +
-        "                </th>\n" +
-        "            </tr>\n" +
-        "            <tr>\n" +
-        "                <td class=\"col1\">\n" +
-        "                    <span></span>\n" +
-        "                </td>\n" +
-        "                <td class=\"col4\">\n" +
-        "                    <span></span>\n" +
-        "                    <!--                    <span class=\"sub-temp\"></span>-->\n" +
-        "                    <!--                    <span class=\"sub-temp sub-temp-three\"></span>-->\n" +
-        "                </td>\n" +
-        "                <td class=\"col5\">\n" +
-        "                    <span></span>\n" +
-        "                </td>\n" +
-        "            </tr>\n" +
-        "\n" +
-        "            <tr>\n" +
-        "                <td class=\"col1\">\n" +
-        "                    <span></span>\n" +
-        "                </td>\n" +
-        "                <td class=\"col4\">\n" +
-        "                    <span></span>\n" +
-        "                </td>\n" +
-        "                <td class=\"col5\">\n" +
-        "                    <span></span>\n" +
-        "                </td>\n" +
-        "            </tr>\n" +
-        "\n" +
-        "            <tr>\n" +
-        "                <td class=\"col1\">\n" +
-        "                    <span></span>\n" +
-        "                </td>\n" +
-        "                <td class=\"col4\">\n" +
-        "                    <span></span>\n" +
-        "                </td>\n" +
-        "                <td class=\"col5\">\n" +
-        "                    <span></span>\n" +
-        "                </td>\n" +
-        "            </tr>\n" +
-        "\n" +
-        "        </table>"
+  return " <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" class=\"w3samples_table_loader\">\n" +
+    "            <tr>\n" +
+    "                <th class=\"col1\">\n" +
+    "                    <span></span>\n" +
+    "                </th>\n" +
+    "                <th class=\"col4\">\n" +
+    "                    <span></span>\n" +
+    "                </th>\n" +
+    "                <th class=\"col5\">\n" +
+    "                    <span></span>\n" +
+    "                </th>\n" +
+    "            </tr>\n" +
+    "            <tr>\n" +
+    "                <td class=\"col1\">\n" +
+    "                    <span></span>\n" +
+    "                </td>\n" +
+    "                <td class=\"col4\">\n" +
+    "                    <span></span>\n" +
+    "                    <!--                    <span class=\"sub-temp\"></span>-->\n" +
+    "                    <!--                    <span class=\"sub-temp sub-temp-three\"></span>-->\n" +
+    "                </td>\n" +
+    "                <td class=\"col5\">\n" +
+    "                    <span></span>\n" +
+    "                </td>\n" +
+    "            </tr>\n" +
+    "\n" +
+    "            <tr>\n" +
+    "                <td class=\"col1\">\n" +
+    "                    <span></span>\n" +
+    "                </td>\n" +
+    "                <td class=\"col4\">\n" +
+    "                    <span></span>\n" +
+    "                </td>\n" +
+    "                <td class=\"col5\">\n" +
+    "                    <span></span>\n" +
+    "                </td>\n" +
+    "            </tr>\n" +
+    "\n" +
+    "            <tr>\n" +
+    "                <td class=\"col1\">\n" +
+    "                    <span></span>\n" +
+    "                </td>\n" +
+    "                <td class=\"col4\">\n" +
+    "                    <span></span>\n" +
+    "                </td>\n" +
+    "                <td class=\"col5\">\n" +
+    "                    <span></span>\n" +
+    "                </td>\n" +
+    "            </tr>\n" +
+    "\n" +
+    "        </table>"
 }
 
 
